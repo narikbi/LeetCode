a0ec62e91d04ca2309a61a167af76b0b3abb6689
Narikbi <narikbi@gmail.com>
2017-03-18 12:31:50 +0600

    257 task bugfix.

--- source/257.cpp
+++ source/257.cpp
@@ -7,3 +7,47 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <queue>
+#include <set>
+#include <map>
+#include <stack>
+#include <cmath>
+#include <numeric>
+#include <unordered_map>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+vector<string> res;
+
+void rec(TreeNode *node, string path) {
+    
+    if (node->left == NULL && node->right == NULL) {
+        res.push_back(path + to_string(node->val));
+    }
+    
+    if (node->left != NULL) rec(node->left, path + to_string(node->val) + "->");
+    if (node->right != NULL) rec(node->right, path + to_string(node->val) + "->");
+    
+}
+
+vector<string> binaryTreePaths(TreeNode* root) {
+    
+    if (root != NULL) rec(root, {});
+    
+    return res;
+}
+
+
+
